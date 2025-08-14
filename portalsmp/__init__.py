cec5dcc38d0218462c9d0a2a43c7128c8cbc9173
savv <85193581+savfrmda3@users.noreply.github.com>
2025-08-15 01:10:53 +0300

    Create __init__.py

---
+++ portalsmp/__init__.py
@@ -0,0 +1,25 @@
+from portalsmp.portalsapi import *
+
+__all__ = [
+    "cap",
+    "listToURL",
+    "update_auth",
+    "search",
+    "giftsFloors",
+    "myPortalsGifts",
+    "myPoints",
+    "myBalances",
+    "myActivity",
+    "collections",
+    "marketActivity",
+    "convertForListing",
+    "convertForBuying",
+    "bulkList",
+    "sale",
+    "buy",
+    "makeOffer",
+    "cancelOffer",
+    "changePrice",
+    "PortalsGift",
+    "withdrawPortals"
+]
